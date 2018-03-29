8512860d8d303d174212d29864f73e0cb7edc36f
aakashtiwari <akashtiwari82@gmail.com>
2018-03-30 01:37:35 +0530

    adds show method

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,6 +3,11 @@ class SongsController < ApplicationController
   # For APIs, you may want to use :null_session instead.
   def index
   	songs = Song.all
-	  render json: { data: songs }, status: 200
+	render json: { data: songs }, status: 200
+  end
+
+  def show
+  	song = Song.find(params[:id])
+  	render json: { data: song }, status: 200
   end
 end
