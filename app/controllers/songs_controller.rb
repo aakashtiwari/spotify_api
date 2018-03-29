a7c5835124bdafc8a2b19edbe2aad7723d6ad27c
Aakash Tiwari <akashtiwari82@gmail.com>
2018-03-29 18:08:43 +0530

    changes data return key

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,8 +2,7 @@ class SongsController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   def index
-  	binding.pry
   	songs = Song.all
-	  render json: { songs: songs }, status: 200
+	  render json: { data: songs }, status: 200
   end
 end
